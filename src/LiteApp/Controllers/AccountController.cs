6781ca216d20d2321549d50a77765fbabe72c841
Ardalan Malihi <ardalan.malihi@outlook.com>
2016-09-04 15:46:08 -0700

    Adding Auth0

---
+++ src/LiteApp/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LiteApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+
+        [Authorize]
+        public IActionResult Logout()
+        {
+            HttpContext.Authentication.SignOutAsync("Auth0");
+            HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "App");
+        }
+    }
+}
+
--- src/LiteApp/Startup.cs
+++ src/LiteApp/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using LiteApp.Data;
 using LiteApp.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace LiteApp
 {
@@ -32,12 +35,22 @@ namespace LiteApp
             services.AddSingleton<IAppData, AppData>();
             services.AddSingleton<IAppService, AppService>();
 
+            // Add authentication services
+            services.AddAuthentication(
+                options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+
             // Add framework services.
             services.AddMvc();
+
+            // Add functionality to inject IOptions<T>
+            services.AddOptions();
+
+            // Add the Auth0 Settings object so it can be injected
+            services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IOptions<Auth0Settings> auth0Settings)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -54,12 +67,50 @@ namespace LiteApp
 
             app.UseStaticFiles();
 
+            // Add the cookie middleware
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true
+            });
+
+            // Add the OIDC middleware
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions("Auth0")
+            {
+                // Set the authority to your Auth0 domain
+                Authority = $"https://{auth0Settings.Value.Domain}",
+
+                // Configure the Auth0 Client ID and Client Secret
+                ClientId = auth0Settings.Value.ClientId,
+                ClientSecret = auth0Settings.Value.ClientSecret,
+
+                // Do not automatically authenticate and challenge
+                AutomaticAuthenticate = false,
+                AutomaticChallenge = false,
+
+                // Set response type to code
+                ResponseType = "code",
+
+                // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
+                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
+                CallbackPath = new PathString("/signin-auth0"),
+
+                // Configure the Claims Issuer to be Auth0
+                ClaimsIssuer = "Auth0"
+            });
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     "Page",
                     "{page}",
                     new { controller = "App", action = "Index", page= "Home" }
+                );
+
+                routes.MapRoute(
+                    "Account",
+                    "{controller}/{action}",
+                    new { controller = "Account" }
                 );
             });
         }
